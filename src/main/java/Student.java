57f939a5469f3bf5bdf1becc45dfef3a62dc961b
Rachel Jaisey <jaiseyrachel@gmail.com>
2024-07-24 18:42:50 +0100

    feat: add name and yearGroup attributes and create constructor

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,2 +1,9 @@
 public class Student {
+    private String name;
+    private String yearGroup;
+
+    public Student(String name, String yearGroup) {
+        this.name = name;
+        this.yearGroup = yearGroup;
+    }
 }
