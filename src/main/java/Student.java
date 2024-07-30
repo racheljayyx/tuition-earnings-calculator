db890c121d385e925daa169eff5fc4c00d46fc31
Rachel Jaisey <jaiseyrachel@gmail.com>
2024-07-30 17:34:36 +0100

    fix: update student class to take take first name and last name separately

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,9 +1,16 @@
 public class Student {
-    private String name;
+    private String firstName;
+    private String lastName;
     private String yearGroup;
 
-    public Student(String name, String yearGroup) {
-        this.name = name;
+
+    public Student(String firstName, String lastName, String yearGroup) {
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.yearGroup = yearGroup;
     }
+
+
+
+
 }
