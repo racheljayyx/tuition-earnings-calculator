63cda6beb6990facc9598211c4b76ca3ed4c831a
Rachel Jaisey <jaiseyrachel@gmail.com>
2024-07-30 18:10:04 +0100

    feat: add firstName, lastName, and payBand and constructor

--- src/main/java/Tutor.java
+++ src/main/java/Tutor.java
@@ -1,2 +1,13 @@
 public class Tutor {
+    private String firstName;
+    private String lastName;
+    private String payBand;
+
+    public Tutor(String firstName, String lastName, String payBand) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.payBand = payBand;
+    }
+
+
 }
